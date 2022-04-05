b486d0d5ac75ad1b405c3619445341f5091e02f3
Kasraoui Anouar <med.anouar91@gmail.com>
2022-04-05 16:02:59 +0200

    components

---
+++ src/profile/Adresse.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Adresse =()=>{
+    return(
+        <h2>Zarzouna Bizerte 7021</h2>
+    )
+}
+export default Adresse
---
+++ src/profile/ProfilePhoto.js
@@ -0,0 +1,7 @@
+import React from "react";
+const Photo =()=>{
+    return (
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVA_HrQLjkHiJ2Ag5RGuwbFeDKRLfldnDasw&usqp=CAU"/>
+    )
+}   
+export default Photo;
